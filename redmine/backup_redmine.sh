6affb0ee7d73d1ded05c91d3f1127c5f88b4397c
Augusto W. Andreoli <andreoliwa@gmail.com>
2021-04-17 16:02:01 +0200

    ci: add pre-commit config

--- redmine/backup_redmine.sh
+++ redmine/backup_redmine.sh
@@ -14,5 +14,8 @@ set -e
 # Redmine backup on ~/OneDrive/Backup/
 source $HOME_DIR/.config/dotfiles/local.env
 echo 'Dumping the database...'
-/usr/local/bin/docker-compose -f $HOME_DIR/container-apps/postgres/docker-compose.yml exec -T postgres12 pg_dump -U postgres redmine > "$BACKUP_DIR/redmine_$(date "+%Y-%m-%d-%H-%M-%S").sql"
+/usr/local/bin/docker-compose -f \
+    $HOME_DIR/container-apps/postgres/docker-compose.yml \
+    exec -T postgres12 pg_dump -U postgres redmine > \
+    "$BACKUP_DIR/redmine_$(date "+%Y-%m-%d-%H-%M-%S").sql"
 ls -l "$BACKUP_DIR"
