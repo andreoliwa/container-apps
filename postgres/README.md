c1e4407d5b635a91b61cea2cb61af1e078438d72
Augusto W. Andreoli <andreoliwa@gmail.com>
2021-10-14 23:10:09 +0200

    feat: upgrade PostgreSQL from 13 to 14

--- postgres/README.md
+++ postgres/README.md
@@ -2,7 +2,7 @@
 
 ## Dump a single database
 
-To dump a database (with date/time on the file name):
+To dump a database on the backup dir, with date/time on the file name:
 
 ```bash
 cd ~/container-apps
@@ -15,7 +15,7 @@ To restore a database dump:
 
 1. Connect to the container.
    ```bash
-   db exec postgres13 psql -U postgres
+   db exec postgres14 psql -U postgres
    ```
 2. Drop and recreate the database.
    ```sql
@@ -25,7 +25,7 @@ To restore a database dump:
    ```
 3. Exit the container and restore the dump on the newly created database.
    ```bash
-   db exec -T postgres13 psql -U [user_name] < /path/to/dump_of_a_single_database.sql
+   db exec -T postgres14 psql -U [user_name] -f /var/backups/path/to/dump_of_a_single_database.sql
    ```
 
 ## Upgrade Postgres
--- tasks.py
+++ tasks.py
@@ -3,24 +3,27 @@ from invoke import task, Exit
 from datetime import datetime
 from pathlib import Path
 import os
+import socket
 from invoke_home import print_error, run_command
 
 DB_USER = "postgres"
+POSTGRES_VERSION = 14
 POSTGRES_ENV = "POSTGRES_PASSWORD"
 DB_PASSWORD = os.environ[POSTGRES_ENV]
+BACKUP_PATH = Path("~/OneDrive/Backup").expanduser()
 
 
 @task(
     help={
         "database": "Database name",
-        "version": "PostgreSQL version: 13 (default) or 12",
+        "version": f"PostgreSQL version (default: {POSTGRES_VERSION})",
         "psql": "Use psql instead of pgcli",
         "command": "Run a SQL command (only works with psql, ignored on pgcli)",
         "user": f"User name (default: {DB_USER})",
         "password_env": f"Environment variable with the password (default: {POSTGRES_ENV})",
     }
 )
-def db_connect(c, database="postgres", version=13, psql=False, command="", user="", password_env=""):
+def db_connect(c, database="postgres", version=POSTGRES_VERSION, psql=False, command="", user="", password_env=""):
     """Connect to the containerised PostgreSQL database using pgcli."""
     db_user = user or DB_USER
     db_password = os.environ[password_env.upper()] if password_env else DB_PASSWORD
@@ -43,14 +46,21 @@ def db_connect(c, database="postgres", version=13, psql=False, command="", user=
 @task(
     help={
         "database": "Database name",
-        "version": "PostgreSQL version: 13 (default) or 12",
-        "output_dir": "Output directory for the dump file",
+        "version": f"PostgreSQL version (default: {POSTGRES_VERSION})",
+        "output_dir": f"Output directory for the dump file (default: {str(BACKUP_PATH)})",
     }
 )
-def db_dump(c, database, version=13, output_dir="~/Downloads"):
+def db_dump(c, database, version=POSTGRES_VERSION, output_dir=""):
     """Dump a single database (with date/time on the file name)."""
     datetime_str = datetime.now().isoformat().replace(":", "-").split(".")[0]
+
+    if not output_dir:
+        host_name = socket.gethostname().replace(".local", "")
+        output_dir = BACKUP_PATH / host_name / f"postgres{version}"
+
     expanded_dir = Path(output_dir).expanduser()
+    expanded_dir.mkdir(exist_ok=True, parents=True)
+
     full_dump_path = expanded_dir / f"{database}_{datetime_str}.sql"
     c.run(f"docker exec -it postgres{version} pg_dump -U postgres {database} > {full_dump_path}")
     c.run(f"ls -l {str(expanded_dir)}")
