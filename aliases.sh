42700f8721fdf1efe627659bb9e04fc445469861
W. Augusto Andreoli <andreoliwa@gmail.com>
2022-11-07 02:57:19 +0100

    fix: increase connections and buffers
    To avoid psycopg2.OperationalError:
    FATAL:  remaining connection slots
    are reserved for non-replication superuser connections

--- aliases.sh
+++ aliases.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Using the new Docker CLI ("docker compose" without dash)
-alias db='docker compose -f ~/container-apps/postgres/docker-compose.yml'
+alias db='docker compose -f ~/container-apps/postgres/compose.yml'
 alias rss='docker compose -f ~/container-apps/tt-rss/docker-compose.yml'
 alias mayan='docker compose -f ~/container-apps/mayan/docker-compose.yml'
 alias strapi='docker compose -f ~/container-apps/strapi/docker-compose.yml'
