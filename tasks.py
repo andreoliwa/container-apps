2f289c37bdcde3adfa52bb04142a584c2a9d81fa
W. Augusto Andreoli <andreoliwa@gmail.com>
2023-03-29 09:26:57 +0200

    feat: list databases

--- tasks.py
+++ tasks.py
@@ -9,7 +9,6 @@ from conjuring.grimoire import print_error, run_command
 DB_USER = "postgres"
 POSTGRES_VERSION = 14
 POSTGRES_ENV = "POSTGRES_PASSWORD"
-DB_PASSWORD = os.environ.get(POSTGRES_ENV)
 BACKUP_PATH = Path("~/OneDrive/Backup").expanduser()
 
 
@@ -21,16 +20,33 @@ BACKUP_PATH = Path("~/OneDrive/Backup").expanduser()
         "command": "Run a SQL command (only works with psql, ignored on pgcli)",
         "user": f"User name (default: {DB_USER})",
         "password_env": f"Environment variable with the password (default: {POSTGRES_ENV})",
+        "list_": "List databases using psql",
     }
 )
-def db_connect(c, database="postgres", version=POSTGRES_VERSION, psql=False, command="", user="", password_env=""):
+def db_connect(
+    c,
+    database="postgres",
+    version=POSTGRES_VERSION,
+    psql=False,
+    command="",
+    user="",
+    password_env=POSTGRES_ENV,
+    list_=False,
+):
     """Connect to the containerised PostgreSQL database using pgcli."""
     db_user = user or DB_USER
-    db_password = os.environ[password_env.upper()] if password_env else DB_PASSWORD
-    if psql:
+    db_password = os.environ[password_env.upper()]
+    if psql or list_:
+        if list_:
+            command = (
+                "SELECT datname AS database_name FROM pg_database"
+                " WHERE datname NOT IN ('postgres') AND datname NOT LIKE 'template%';"
+            )
+
         run_command(
             c,
-            f"docker exec -it postgres{version} psql -U {db_user}",
+            # f"docker exec -it postgres{version} psql -U {db_user} --help",
+            f"docker exec -it postgres{version} psql -U {db_user} --csv --tuples-only",
             f'--command="{command}"' if command else "",
             database,
         )
@@ -40,6 +56,9 @@ def db_connect(c, database="postgres", version=POSTGRES_VERSION, psql=False, com
             raise Exit(code=1)
 
         port = f"77{version}"
+        # List databases fails on pgcli; I tried --list before and after the connection string:
+        #   connection is bad: No such file or directory
+        #   Is the server running locally and accepting connections on that socket?
         c.run(f"pgcli postgresql://{db_user}:{db_password}@localhost:{port}/{database}")
 
 
@@ -63,7 +82,7 @@ def db_dump(c, database, version=POSTGRES_VERSION, output_dir=""):
 
     full_dump_path = expanded_dir / f"{database}_{datetime_str}.sql"
     c.run(f"docker exec -it postgres{version} pg_dump -U postgres {database} > {full_dump_path}")
-    c.run(f"ls -l {str(expanded_dir)}")
+    c.run(f"ls -l {str(expanded_dir)}", dry=False)
 
 
 @task
