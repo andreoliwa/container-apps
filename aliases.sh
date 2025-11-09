86775f0bfae5fe8c051e383aece317e5a00e81c7
W. Augusto Andreoli <andreoliwa@sent.com>
2025-11-09 22:50:43 +0100

    feat: add Focalboard server

--- aliases.sh
+++ aliases.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
-# Hetzner doesn't suppoert the new Docker CLI ("docker compose" without dash)
+# Hetzner doesn't support the new Docker CLI ("docker compose" without dash)
+# keep-sorted start
 alias db='docker compose -f ~/container-apps/postgres/compose.yml'
-alias rss='docker compose -f ~/container-apps/tt-rss/docker compose.yml'
+alias focalboard='docker compose -f ~/container-apps/focalboard/docker-compose.yml'
 alias mayan='docker compose -f ~/container-apps/mayan/docker compose.yml'
-alias strapi='docker compose -f ~/container-apps/strapi/docker compose.yml'
-alias redmine='docker compose -f ~/container-apps/redmine/docker compose.yml'
-alias wikijs='docker compose -f ~/container-apps/wikijs/docker compose.yml'
 alias mysql='docker compose -f ~/container-apps/mysql/docker compose.yml'
+alias redmine='docker compose -f ~/container-apps/redmine/docker compose.yml'
+alias rss='docker compose -f ~/container-apps/tt-rss/docker compose.yml'
+alias strapi='docker compose -f ~/container-apps/strapi/docker compose.yml'
+alias wikijs='docker compose -f ~/container-apps/wikijs/docker compose.yml'
+# keep-sorted end
---
+++ focalboard/README.md
@@ -0,0 +1,30 @@
+# Focalboard
+
+- [Focalboard - Personal Edition](https://www.focalboard.com/download/personal-edition/docker/)
+- [mattermost-community/focalboard: Focalboard is an open source, self-hosted alternative to Trello, Notion, and Asana](https://github.com/mattermost-community/focalboard)
+- [API Documentation](https://htmlpreview.github.io/?https://github.com/mattermost/focalboard/blob/main/server/swagger/docs/html/index.html)
+
+Focalboard is an open source, multilingual, self-hosted project management tool that's an alternative to Trello, Notion, and Asana.
+
+# Setup
+
+1. Start Focalboard:
+   ```bash
+   focalboard up -d
+   ```
+2. Navigate to http://localhost:8005/ to access your Focalboard server.
+3. Create your first account (the first user becomes the admin).
+4. Start creating boards, cards, and organizing your projects!
+
+# Usage
+
+The data is stored in `~/OneDrive/Apps/Focalboard/` using SQLite database.
+
+# Upgrading
+
+To upgrade to the latest version:
+
+```bash
+focalboard pull
+focalboard up -d
+```
